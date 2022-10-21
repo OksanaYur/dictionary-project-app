058170e26b4b3c1a4451dcbfc79c8122b8f7f861
OksanaYur <107573946+OksanaYur@users.noreply.github.com>
2022-10-21 20:03:53 +0300

    meaning function was added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
-
+import Results from "./Results";
 
 export default function Dictionary () {
 
     let [keyword, setKeyword] = useState(""); 
+    let [results, setResults] = useState(null);
 
 function handleResponse(response) {
-
-
+setResults(response.data[0]);
 }
 
 function search(event) {
-    event.preventDefault()
+    event.preventDefault();
+    //documentation https://dictionaryapi.dev
 
 let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 axios.get(apiUrl).then(handleResponse);
 }
 
 
-function updateKeywordChange(event) {
-setKeyword(event.target.value)
+function handleKeywordChange(event) {
+setKeyword(event.target.value);
 }
 
     return (
         <div className="Dictionary">
 <form className="form" onSubmit={search}>
-        <input type="search" onChange={updateKeywordChange} />
+        <input type="search" onChange={handleKeywordChange} />
 </form>
+<Results results={results} />
         </div>
     )
 }
