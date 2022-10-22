90741967226a11a550ec3b59c348e5c59369604c
OksanaYur <107573946+OksanaYur@users.noreply.github.com>
2022-10-22 19:06:46 +0300

    code update for result showing

--- src/Results.js
+++ src/Results.js
@@ -4,19 +4,20 @@ import Meaning from "./Meaning";
 export default function Result(props) {
     console.log(props.results);
     if (props.results) {
-return (
-<div className="Results">
-    <h2> {props.results.word} </h2>
-    {props.reults.meanings.map(function (meaning, index) {
         return (
-        <div key={index}>
-            <Meaning meaning={meaning} />
-            </div>
-    );
-    })}
-</div>
-);
-    } else {
+          <div className="Results">
+            <h2> {props.results.word} </h2>
+            {props.results.meanings.length !== 0 &&
+              props.results.meanings.map(function (meaning, index) {
+                return (
+                  <div key={index}>
+                    <Meaning meaning={meaning} />
+                  </div>
+                );
+              })}
+          </div>
+        );
+      } else {
         return null;
     }
 }
